b43b338ac8ba01a5466b92c1fd0e307da31fd02a
guaifish <guaifish@hotmail.com>
2020-09-07 15:43:16 +0800

    soa_derive

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
-use displaydoc::Display;
+use soa_derive::StructOfArray;
 
-/// p({x:?}, {y:?})
-#[derive(Debug, Display)]
-struct Point {
-    x: i32,
-    y: i32,
+#[derive(Debug, Clone, PartialEq, StructOfArray)]
+#[soa_derive = "Debug, Clone, PartialEq"]
+pub struct Particle {
+    pub name: String,
+    pub mass: f64,
+}
+
+impl Particle {
+    pub fn new(name: String, mass: f64) -> Self {
+        Particle {
+            name: name,
+            mass: mass,
+        }
+    }
 }
 
 fn main() {
-    let p1 = Point{ x: 1, y: 2 };
-    println!("{}", p1);
+    let mut particles = ParticleVec::new();
+    particles.push(Particle::new(String::from("Cl"), 0.0));
+    particles.push(Particle::new(String::from("Na"), 0.0));
+    particles.push(Particle::new(String::from("Br"), 0.0));
+    particles.push(Particle::new(String::from("Zn"), 0.0));
+
+    println!("{:?}", particles);
 }
