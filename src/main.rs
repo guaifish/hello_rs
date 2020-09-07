3894d0a390ae3ea9fb1da2075ec9ef8443f9a8a2
guaifish <guaifish@hotmail.com>
2020-09-07 16:17:45 +0800

    prettytable-rs

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,9 @@
-use soa_derive::StructOfArray;
-
-#[derive(Debug, Clone, PartialEq, StructOfArray)]
-#[soa_derive = "Debug, Clone, PartialEq"]
-pub struct Particle {
-    pub name: String,
-    pub mass: f64,
-}
-
-impl Particle {
-    pub fn new(name: String, mass: f64) -> Self {
-        Particle {
-            name: name,
-            mass: mass,
-        }
-    }
-}
+#[macro_use] extern crate prettytable;
 
 fn main() {
-    let mut particles = ParticleVec::new();
-    particles.push(Particle::new(String::from("Cl"), 0.0));
-    particles.push(Particle::new(String::from("Na"), 0.0));
-    particles.push(Particle::new(String::from("Br"), 0.0));
-    particles.push(Particle::new(String::from("Zn"), 0.0));
+    let table = table!(["ABC", "DEFG", "HIJKLMN"],
+                       ["foobar", "bar", "foo"],
+                       ["foobar2", "bar2", "foo2"]);
 
-    println!("{:?}", particles);
+    table.printstd();
 }
