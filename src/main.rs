a4424ac0dd32a6bb215c0f97d70d9ad2315abae5
guaifish <guaifish@hotmail.com>
2020-09-07 15:22:24 +0800

    displaydoc struct

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,13 @@
 use displaydoc::Display;
 
+/// p({x:?}, {y:?})
 #[derive(Debug, Display)]
-pub enum DataStoreError {
-    /// data store disconnected
-    Disconnect,
-    /// the data for key `{0}` is not available
-    Redaction(String),
-    /// invalid header (expected {expected:?}, found {found:?})
-    InvalidHeader { expected: String, found: String },
-    /// unknown data store error
-    Unknown,
+struct Point {
+    x: i32,
+    y: i32,
 }
 
 fn main() {
-    let disconnect = DataStoreError::Disconnect;
-    println!(
-        "Enum value `Disconnect` should be printed as:\n\t{}",
-        disconnect
-    );
-
-    let redaction = DataStoreError::Redaction(String::from("Dummy"));
-    println!(
-        "Enum value `Redaction` should be printed as:\n\t{}",
-        redaction
-    );
-
-    let invalid_header = DataStoreError::InvalidHeader {
-        expected: String::from("https"),
-        found: String::from("http"),
-    };
-    println!(
-        "Enum value `InvalidHeader` should be printed as:\n\t{}",
-        invalid_header
-    );
+    let p1 = Point{ x: 1, y: 2 };
+    println!("{}", p1);
 }
