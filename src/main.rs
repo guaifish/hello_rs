32bc45d25e3ddddfcd9c61e7c36b277550bea82c
guaifish <guaifish@hotmail.com>
2020-09-07 15:00:28 +0800

    derive_new

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,16 @@
-use serde::{Serialize, Deserialize};
+#[macro_use]
+extern crate derive_new;
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Point {
-    x: i32,
+#[derive(new, Debug)]
+struct Foo {
+    x: bool,
+    #[new(value = "42")]
     y: i32,
+    #[new(default)]
+    z: Vec<String>,
 }
 
 fn main() {
-    let point = Point { x: 1, y: 2 };
-
-    let serialized = serde_json::to_string(&point).unwrap();
-
-    println!("serialized = {}", serialized);
-
-    let deserialized: Point = serde_json::from_str(&serialized).unwrap();
-
-    println!("deserialized = {:?}", deserialized);
+    let a = Foo::new(true);
+    println!("{:?}", a);
 }
