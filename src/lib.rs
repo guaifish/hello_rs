f58f4ca0c3b6e811f2a37e90f6c1fb28b78e8340
guaifish <guaifish@hotmail.com>
2020-09-09 11:02:21 +0800

    attribute macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
 
-#[proc_macro_derive(AnswerFn)]
-pub fn derive_answer_fn(_item: TokenStream) -> TokenStream {
-    "fn answer() -> u32 { 42 }".parse().unwrap()
+#[proc_macro_attribute]
+pub fn show_streams(attr: TokenStream, item: TokenStream) -> TokenStream {
+    println!("attr: \"{}\"", attr.to_string());
+    println!("item: \"{}\"", item.to_string());
+    item
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
-use hello::AnswerFn;
+use hello::show_streams;
 
-#[derive(AnswerFn)]
-struct Struct;
+#[show_streams]
+fn invoke1() {}
+// out: attr: ""
+// out: item: "fn invoke1() { }"
+
+// Example: Attribute with input
+#[show_streams(bar)]
+fn invoke2() {}
+// out: attr: "bar"
+// out: item: "fn invoke2() {}"
+
+// Example: Multiple tokens in the input
+#[show_streams(multiple => tokens)]
+fn invoke3() {}
+// out: attr: "multiple => tokens"
+// out: item: "fn invoke3() {}"
+
+// Example:
+#[show_streams { delimiters }]
+fn invoke4() {}
+// out: attr: "delimiters"
+// out: item: "fn invoke4() {}"
 
 fn main() {
-    assert_eq!(42, answer());
 }
