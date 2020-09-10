6da58129e6c522cff40de8589328dfcf2448eed2
guaifish <guaifish@hotmail.com>
2020-09-11 00:04:23 +0800

    fnv hashmap

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,12 @@
-extern crate deunicode;
-
-use deunicode::deunicode;
+use fnv::FnvHashMap;
 
 fn main() {
-    assert_eq!(deunicode("Æneid"), "AEneid");
-    assert_eq!(deunicode("étude"), "etude");
-    assert_eq!(deunicode("北亰"), "Bei Jing");
-    assert_eq!(deunicode("ᔕᓇᓇ"), "shanana");
-    assert_eq!(deunicode("げんまい茶"), "genmaiCha");
-    assert_eq!(deunicode("🦄☣"), "unicorn biohazard");
-    assert_eq!(deunicode("…"), "...");
-    
-    assert_eq!(deunicode("你好!"), "Ni Hao !");
-    assert_eq!(deunicode("囧"), "Jiong");
-    assert_eq!(deunicode("〇"), "0");
-    assert_eq!(deunicode("零"), "Ling");
+    let mut map = FnvHashMap::default();
+    map.insert(1, "one");
+    map.insert(2, "two");
+
+    map = FnvHashMap::with_capacity_and_hasher(10, Default::default());
+    map.insert(1, "one");
+    map.insert(2, "two");
+    println!("{:?}", map);
 }
