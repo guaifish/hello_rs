a188fc0837d8e94ef80032dac2814fbf9e860790
guaifish <guaifish@hotmail.com>
2020-09-10 23:08:26 +0800

    typenum

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
-use generic_array::{typenum::U5, ArrayLength, GenericArray};
-
-#[derive(Debug)]
-struct Foo<N: ArrayLength<i32>> {
-    data: GenericArray<i32, N>,
-}
+use std::ops::Add;
+use typenum::{Integer, P3, P4};
 
 fn main() {
-    let foo = Foo::<U5> {
-        data: GenericArray::default(),
-    };
-    println!("{:?}", foo);
+    type X = <P3 as Add<P4>>::Output;
+    assert_eq!(<X as Integer>::to_i32(), 7);
 }
