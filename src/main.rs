6a64909d54c4a92dcbb55256546129ea32c92b6c
guaifish <guaifish@hotmail.com>
2020-09-10 17:24:14 +0800

    chrono date

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,9 @@
-use bumpalo::{boxed::Box, Bump};
-use std::sync::atomic::{AtomicUsize, Ordering};
-
-static NUM_DROPPED: AtomicUsize = AtomicUsize::new(0);
-
-struct CountDrops;
-
-impl Drop for CountDrops {
-    fn drop(&mut self) {
-        NUM_DROPPED.fetch_add(1, Ordering::SeqCst);
-    }
-}
-
 fn main() {
-    // Create a new bump arena.
-    let bump = Bump::new();
+    use chrono::prelude::*;
 
-    // Create a `CountDrops` inside the bump arena.
-    let c = Box::new_in(CountDrops, &bump);
+    let utc: DateTime<Utc> = Utc::now();
+    let local: DateTime<Local> = Local::now();
 
-    // No `CountDrops` have been dropped yet.
-    assert_eq!(NUM_DROPPED.load(Ordering::SeqCst), 0);
-
-    // Drop our `Box<CountDrops>`.
-    drop(c);
-
-    // Its `Drop` implementation was run, and so `NUM_DROPS` has been incremented.
-    assert_eq!(NUM_DROPPED.load(Ordering::SeqCst), 1);
+    println!("{}", utc);
+    println!("{}", local);
 }
