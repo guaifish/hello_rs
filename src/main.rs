be8df2e4fe8fa4f1b393bdbc0e25bad9e7982fea
guaifish <guaifish@hotmail.com>
2020-09-10 23:30:13 +0800

    deunicode

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
-use const_random::const_random;
+extern crate deunicode;
 
-const MY_RANDOM_NUMBER: u32 = const_random!(u32);
+use deunicode::deunicode;
 
 fn main() {
-    println!("{}", MY_RANDOM_NUMBER);
+    assert_eq!(deunicode("Æneid"), "AEneid");
+    assert_eq!(deunicode("étude"), "etude");
+    assert_eq!(deunicode("北亰"), "Bei Jing");
+    assert_eq!(deunicode("ᔕᓇᓇ"), "shanana");
+    assert_eq!(deunicode("げんまい茶"), "genmaiCha");
+    assert_eq!(deunicode("🦄☣"), "unicorn biohazard");
+    assert_eq!(deunicode("…"), "...");
+    
+    assert_eq!(deunicode("你好!"), "Ni Hao !");
+    assert_eq!(deunicode("囧"), "Jiong");
+    assert_eq!(deunicode("〇"), "0");
+    assert_eq!(deunicode("零"), "Ling");
 }
