15baedb56d06d97751361d506a6336bf614a211d
guaifish <guaifish@hotmail.com>
2020-09-10 15:40:38 +0800

    base-x

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use base64::{encode, decode};
+use base_x::{encode, decode};
 
 fn main() {
-    let a = b"hello world";
-    let b = "aGVsbG8gd29ybGQ=";
-
-    assert_eq!(encode(a), b);
-    assert_eq!(a, &decode(b).unwrap()[..]);
+    let alphabet = "0123456789";
+    let a = b"Hello, World!";
+    let b = "5735816763073854918203775149089";
+    assert_eq!(encode(alphabet, a), b);
+    assert_eq!(a, &decode(alphabet, b).unwrap()[..]);
 }
