40d09e0b663d857c1d90117ca18cbc44dfed5af9
guaifish <guaifish@hotmail.com>
2020-09-14 00:45:07 +0800

    deque

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,27 @@
+use deque;
+
 fn main() {
-    // let a = 3 ** 6;
-    let a = 3_i32.pow(2);
-    println!("{}", a);
-    let a = 23_f64 / 7_f64;
-    println!("{}", a);
-    let _ = 1f64;
-    // 字符串连接和复制
-    let a = "Alice";
-    let b = "Bob";
-    let c = a.to_owned() + b;
-    println!("{} {} {}", a, b, c);
-    let d = a.repeat(3);
-    println!("{}", d.len());
-    let a = "123";
-    let b = "3.14";
-    let a: i32 = a.parse().unwrap();
-    let b: f64 = b.parse().unwrap();
-    assert_eq!(a, 123);
-    assert_eq!(b, 3.14);
-    assert_eq!(format!("{}", a), "123");
+    let (worker, stealer) = deque::new();
+
+    // Only the worker may push/pop
+    worker.push(1);
+    println!("{:?}", worker);
+    worker.pop();
+    println!("{:?}", worker);
+
+    // Stealers take data from the other end of the deque
+    worker.push(1);
+    println!("{:?}", worker);
+
+    stealer.steal();
+    println!("{:?}", worker);
+
+    // Stealers can be cloned to have many stealers stealing in parallel
+    worker.push(1);
+    println!("{:?}", worker);
+
+    let stealer2 = stealer.clone();
+    stealer2.steal();
+    println!("{:?}", worker);
+
 }
