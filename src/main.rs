88b39c2527a5c6d4c88a0d281c32e32362cc95b8
guaifish <guaifish@hotmail.com>
2020-09-14 00:58:05 +0800

    fixed-vec-deque

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,10 @@
-use deque;
+use fixed_vec_deque::FixedVecDeque;
 
 fn main() {
-    let (worker, stealer) = deque::new();
-
-    // Only the worker may push/pop
-    worker.push(1);
-    println!("{:?}", worker);
-    worker.pop();
-    println!("{:?}", worker);
-
-    // Stealers take data from the other end of the deque
-    worker.push(1);
-    println!("{:?}", worker);
-
-    stealer.steal();
-    println!("{:?}", worker);
-
-    // Stealers can be cloned to have many stealers stealing in parallel
-    worker.push(1);
-    println!("{:?}", worker);
-
-    let stealer2 = stealer.clone();
-    stealer2.steal();
-    println!("{:?}", worker);
-
+    let mut q = FixedVecDeque::<[i32; 3]>::new();
+    *q.push_back() = 1;
+    *q.push_back() = 2;
+    *q.push_back() = 3;
+    *q.push_front() = 4;
+    println!("{:?}", q);
 }
