e234ccb20283e5fd08c3ff329c1d78b83c52ea81
guaifish <guaifish@hotmail.com>
2020-09-29 09:51:43 +0800

    reverse bits

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,12 @@
-use bit_vec::BitVec;
-
 fn main() {
-    let mut bv = BitVec::from_elem(10, false);
-
-    // insert all primes less than 10
-    bv.set(2, true);
-    bv.set(3, true);
-    bv.set(5, true);
-    bv.set(7, true);
-    println!("{:?}", bv);
-    println!(
-        "total bits set to true: {}",
-        bv.iter().filter(|x| *x).count()
-    );
-
-    // flip all values in bitvector, producing non-primes less than 10
-    bv.negate();
-    println!("{:?}", bv);
-    println!(
-        "total bits set to true: {}",
-        bv.iter().filter(|x| *x).count()
-    );
-
-    // reset bitvector to empty
-    bv.clear();
-    println!("{:?}", bv);
-    println!(
-        "total bits set to true: {}",
-        bv.iter().filter(|x| *x).count()
-    );
+    let a: u8 = 0b10001010;
+    let b = reverse_bits(a);
+    println!("{:b}", b);
+}
+fn reverse_bits(byte: u8) -> u8 {
+    let mut result = 0;
+    for i in 0..8 {
+        result |= ((byte >> i) & 1) << (8 - 1 - i);
+    }
+    result
 }
